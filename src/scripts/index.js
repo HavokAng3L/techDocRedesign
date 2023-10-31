b956626e809b35f336f3bbcbd50f9ce01451ba7f
HavokAng3L <angelmorin75@gmail.com>
2023-10-31 18:46:09 -0500

    Implemented Styling

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,22 +1,32 @@
 //  Source Codes
 import {JSNodeSource, CppNodeSource, JSInstructionsSource, CppInstructionsSource} from "./SourceCodes.js";
 
-const implementSection = document.getElementById('imp-display')
-const instructionSection = document.getElementById('inst-display')
+const implementSection = document.getElementById('imp-display');
+const instructionSection = document.getElementById('inst-display');
 
+//  Will refactor over time.
 function renderCode(){
+    //  Sets the displays to none on initial loading
+    document.getElementById("imp-render").style.display = "none";
+    document.getElementById("inst-render").style.display = "none";
+
     document.addEventListener('click', (event)=>{
+        //  Depending on what button is clicked, adds textContent (source code) and toggles display
         if(event.target.id === 'JS_imp_button') {
             implementSection.textContent = JSNodeSource;
+            document.getElementById("imp-render").style.display = "block";
         }
         else if(event.target.id === 'Cpp_imp_button') {
             implementSection.textContent = CppNodeSource;
+            document.getElementById("imp-render").style.display = "block";
         }
         else if(event.target.id === 'JS_inst_button') {
             instructionSection.textContent = JSInstructionsSource;
+            document.getElementById("inst-render").style.display = "block";
         }
         else if(event.target.id === 'Cpp_inst_button') {
             instructionSection.textContent = CppInstructionsSource;
+            document.getElementById("inst-render").style.display = "block";
         }
     })
 }
